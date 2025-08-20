495a2d14314041509c13e6a4861d2102bf933773
Kurt Joshua Cayaga <kurtcayagatash@gmail.com>
2025-08-20 22:07:47 +0800

    feat: added dark mode

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './header.scss'
 })
 export class Header {
+  isDark = false;
 
+  toggleTheme() {
+    this.isDark = !this.isDark;
+    document.documentElement.setAttribute('data-theme', this.isDark ? 'dark' : 'light');
+  }
 }
