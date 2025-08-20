1914cb3519ef7bcb5db38164575276fe0c1ead39
Kurt Joshua Cayaga <kurtcayagatash@gmail.com>
2025-08-20 20:38:33 +0800

    update card list to display

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
 import { ContactForm } from './shared/components/contact-form/contact-form';
+import { CardList } from './shared/components/card-list/card-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, ContactForm],
+  imports: [RouterOutlet, Header, ContactForm, CardList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/shared/components/card-list/card-list.ts
+++ src/app/shared/components/card-list/card-list.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ContactService } from '../../services/contact.service';
 
 @Component({
   selector: 'app-card-list',
@@ -7,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './card-list.scss'
 })
 export class CardList {
+  constructor(public contactService: ContactService) {}
 
+  get contacts() {
+    return this.contactService.getContacts();
+  }
+
+  editContact(index: number) {
+    this.contactService.editContact(index);
+  }
+
+  deleteContact(index: number) {
+    this.contactService.deleteContact(index);
+  }
 }
